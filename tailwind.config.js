0c1edaa519bf8aff9202cb29800785fa8e3c6c59
Natsaran Sae-oueng <69569080+Nxts0@users.noreply.github.com>
2024-02-04 22:15:31 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],  theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [require("daisyui")],}
+
