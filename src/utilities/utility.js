ce232996c51ae2b668e11838f534e497434635a0
Natsaran Sae-oueng <69569080+Nxts0@users.noreply.github.com>
2024-02-21 15:28:09 +0700

    Delete src directory

--- src/utilities/utility.js
+++
@@ -1,9 +0,0 @@
-const randomNumber = (max) => {
-  return Math.floor(Math.random() * max)
-}
-
-const randomColor = () =>{
-  return "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
-
-export { randomNumber , randomColor }
